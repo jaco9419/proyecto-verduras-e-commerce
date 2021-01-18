69ec616d92cf01ed0829b767e98a2828af9322fa
jaco9419 <jaco.9419@gmail.com>
2021-01-17 21:09:37 -0300

    Set up SearchBar

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import "./style/SearchBar.css";
+import SearchIcon from '@material-ui/icons/Search';
 
 function SearchBar() {
-    return <div></div>;
+    return <div className="search__bar__container">
+        <div className="search__bar">
+            <input className="search__bar__input" type="text"/>
+            <SearchIcon className="search__icon" />
+        </div>
+        
+    </div>;
 }
 
 export default SearchBar;
