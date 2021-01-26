5a463060106373efb605ec7ae927e91ede2b1403
jaco9419 <jaco.9419@gmail.com>
2021-01-26 09:22:19 -0300

    Add props to ProductosLanding.js

--- src/Landing.js
+++ src/Landing.js
@@ -2,8 +2,7 @@ import React from 'react';
 import './style/Landing.css';
 import SearchBar from './SearchBar';
 import data from './API/data';
-import MinusIcon from '@material-ui/icons/Remove';
-import PlusIcon from '@material-ui/icons/Add';
+import ProductosLanding from './ProductosLanding';
 
 function Landing() {
     return (
@@ -15,51 +14,15 @@ function Landing() {
             </div>
             <SearchBar />
             <div className="products__container">
-                {data.map((product, i) => (
-                    <div className="product" key={i}>
-                        <img
-                            className="product__img"
-                            src={product.src}
-                            alt={product.title}
-                        />
-                        <div className="product__line"></div>
-
-                        <div className="product__subcontainer">
-                            <div className="product__description">
-                                <p className="product__title">
-                                    {product.title}
-                                </p>
-                                <p className="product__unit">
-                                    Unidad: {product.unidad}
-                                </p>
-
-                                {
-                                    product.precio ? 
-                                    <p className="product__price">
-                                        <span className="product__price__extra">$</span>
-                                        {product.precio}
-                                        <span className="product__price__extra"> por unidad</span>
-                                    </p> : <div></div>
-                                }
-                                
-                            </div>
-                            <div className="product__buttons">
-                                <div className="btn__quantity__box">
-                                    <button className="btn__quantity btn__control btn__decrease">
-                                        <MinusIcon className="minus__icon btn__quantity btn__control" />
-                                    </button>
-                                    <p className="product__quantity">3</p>
-                                    <button className="btn__control btn__increase btn__quantity">
-                                        <PlusIcon className="plus__icon btn__quantity btn__control" />
-                                    </button>
-                                </div>
-                                <button className="btn__quantity btn__add">
-                                    Agregar
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+            {data.map((product, i) => (
+                <ProductosLanding 
+                    src={product.src}
+                    title={product.title}
+                    key={i}
+                    unidad={product.unidad}
+                    precio={product.precio}
+                />
+            ))}
             </div>
 
             <footer className="footer">
---
+++ src/ProductosLanding.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import MinusIcon from '@material-ui/icons/Remove';
+import PlusIcon from '@material-ui/icons/Add';
+
+function ProductosLanding({ src, title, key, unidad, precio }) {
+    return <div className="product" key={key}>
+            <img
+                className="product__img"
+                src={src}
+                alt={title}
+            />
+            <div className="product__line"></div>
+
+            <div className="product__subcontainer">
+                <div className="product__description">
+                    <p className="product__title">
+                        {title}
+                    </p>
+                    <p className="product__unit">
+                        Unidad: {unidad}
+                    </p>
+
+                    {
+                        precio ? 
+                        <p className="product__price">
+                            <span className="product__price__extra">$</span>
+                            {precio}
+                            <span className="product__price__extra"> por unidad</span>
+                        </p> : <div></div>
+                    }
+                    
+                </div>
+                <div className="product__buttons">
+                    <div className="btn__quantity__box">
+                        <button className="btn__quantity btn__control btn__decrease">
+                            <MinusIcon className="minus__icon btn__quantity btn__control" />
+                        </button>
+                        <p className="product__quantity">3</p>
+                        <button className="btn__control btn__increase btn__quantity">
+                            <PlusIcon className="plus__icon btn__quantity btn__control" />
+                        </button>
+                    </div>
+                    <button className="btn__quantity btn__add">
+                        Agregar
+                    </button>
+                </div>
+            </div>
+        </div>
+}
+
+export default ProductosLanding;
