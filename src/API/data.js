ba559592c02881f4198f50a1c480cde1569a6a47
jaco9419 <jaco.9419@gmail.com>
2021-01-26 08:56:09 -0300

    Add price

--- src/API/data.js
+++ src/API/data.js
@@ -3,6 +3,7 @@ const data = [
         src: "https://http2.mlstatic.com/D_NQ_NP_996465-MLA41827779757_052020-V.webp",
         title: "Tomates Redondos",
         unidad: "cajas",
+        precio: "100",
 
     },
     {
--- src/Landing.js
+++ src/Landing.js
@@ -9,42 +9,57 @@ function Landing() {
     return (
         <div className="landing">
             <div className="landing__message">
-                <p>Encontrá las verduras y frutas más frescas y hacé tu pedido</p>
+                <p>
+                    Encontrá las verduras y frutas más frescas y hacé tu pedido
+                </p>
             </div>
             <SearchBar />
             <div className="products__container">
+                {data.map((product, i) => (
+                    <div className="product" key={i}>
+                        <img
+                            className="product__img"
+                            src={product.src}
+                            alt={product.title}
+                        />
+                        <div className="product__line"></div>
 
-                {
-                    data.map((product, i) => (
-                        <div className="product" key={i}>
-                            <img
-                                className="product__img"
-                                src={product.src}
-                                alt={product.title}
-                            />
-                            <div className="product__line"></div>
+                        <div className="product__subcontainer">
+                            <div className="product__description">
+                                <p className="product__title">
+                                    {product.title}
+                                </p>
+                                <p className="product__unit">
+                                    Unidad: {product.unidad}
+                                </p>
 
-                            <div className="product__subcontainer">
-                                <div className="product__description">
-                                    <p className="product__title">{product.title}</p>
-                                    <p className="product__unit">Unidad: {product.unidad}</p>
+                                {
+                                    product.precio ? 
+                                    <p className="product__price">
+                                        <span className="product__price__extra">$</span>
+                                        {product.precio}
+                                        <span className="product__price__extra"> por unidad</span>
+                                    </p> : <div></div>
+                                }
+                                
+                            </div>
+                            <div className="product__buttons">
+                                <div className="btn__quantity__box">
+                                    <button className="btn__quantity btn__control btn__decrease">
+                                        <MinusIcon className="minus__icon btn__quantity btn__control" />
+                                    </button>
+                                    <p className="product__quantity">3</p>
+                                    <button className="btn__control btn__increase btn__quantity">
+                                        <PlusIcon className="plus__icon btn__quantity btn__control" />
+                                    </button>
                                 </div>
-                                <div className="product__buttons">
-                                    <div className="btn__quantity__box">
-                                        <button className="btn__quantity btn__control btn__decrease"><MinusIcon className="minus__icon btn__quantity btn__control"/></button>
-                                        <p className="product__quantity">3</p>
-                                        <button className="btn__control btn__increase btn__quantity"><PlusIcon className="plus__icon btn__quantity btn__control"/></button>
-                                    </div>
-                                    <button className="btn__quantity btn__add">Agregar</button>
-                                </div>
+                                <button className="btn__quantity btn__add">
+                                    Agregar
+                                </button>
                             </div>
                         </div>
-                    ))
-                }
-                
-                
-
-                
+                    </div>
+                ))}
             </div>
 
             <footer className="footer">
