6b50908cc6672df28b233fb14ed7a525c9a40b4c
jaco9419 <jaco.9419@gmail.com>
2021-01-31 22:34:09 -0300

    Start form

--- src/Pedidos.js
+++ src/Pedidos.js
@@ -6,9 +6,9 @@ import PlusIcon from '@material-ui/icons/Add';
 
 function Pedidos() {
     return (
-        <div>
-            <div className="pedidos__message">Confirmá tu Pedido:</div>
-            <div className="pedidos__container">
+        <div className="pedidos__container" >
+            <h2 className="pedidos__message">Confirmá tu Pedido:</h2>
+            <div className="lista__pedidos__container">
 
                 <div className="pedido" key={data[0].id}>
                     <img
@@ -80,12 +80,12 @@ function Pedidos() {
                                 Unidad: {data[0].unidad}
                             </p>
 
-                            {data[1].precio ? (
+                            {data[0].precio ? (
                                 <p className="pedido__price">
                                     <span className="pedido__price__extra">
                                         $
                                     </span>
-                                    {data[1].precio}
+                                    {data[0].precio}
                                     <span className="product__price__extra">
                                         {' '}
                                         por unidad
@@ -97,6 +97,183 @@ function Pedidos() {
                         </div>
 
                         <div className="pedido__buttons">
+
+                            <div className="pedido__btn__quantity__box">
+                                <button
+                                    className="btn__quantity pedido__btn__control btn__decrease"
+                                >
+                                    <MinusIcon className="minus__icon btn__quantity pedido__btn__control" />
+                                </button>
+                                <p className="product__quantity">2</p>
+                                <button
+                                    className="pedido__btn__control btn__increase btn__quantity"
+                                >
+                                    <PlusIcon className="plus__icon btn__quantity pedido__btn__control" />
+                                </button>
+                            </div>
+
+                            <button className="btn__quantity btn__delete">
+                                Borrar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="pedido" key={data[0].id}>
+                    <img
+                        className="pedido__img"
+                        src={data[0].src}
+                        alt={data[0].title}
+                    />
+                    <div className="pedido__line"></div>
+
+                    <div className="pedido__subcontainer">
+                        <div className="pedido__description">
+                            <p className="pedido__title">{data[0].title}</p>
+                            <p className="pedido__unit">
+                                Unidad: {data[0].unidad}
+                            </p>
+
+                            {data[0].precio ? (
+                                <p className="pedido__price">
+                                    <span className="pedido__price__extra">
+                                        $
+                                    </span>
+                                    {data[0].precio}
+                                    <span className="product__price__extra">
+                                        {' '}
+                                        por unidad
+                                    </span>
+                                </p>
+                            ) : (
+                                <div></div>
+                            )}
+                        </div>
+
+                        <div className="pedido__buttons">
+
+                            <div className="pedido__btn__quantity__box">
+                                <button
+                                    className="btn__quantity pedido__btn__control btn__decrease"
+                                >
+                                    <MinusIcon className="minus__icon btn__quantity pedido__btn__control" />
+                                </button>
+                                <p className="product__quantity">2</p>
+                                <button
+                                    className="pedido__btn__control btn__increase btn__quantity"
+                                >
+                                    <PlusIcon className="plus__icon btn__quantity pedido__btn__control" />
+                                </button>
+                            </div>
+
+                            <button className="btn__quantity btn__delete">
+                                Borrar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="pedido" key={data[0].id}>
+                    <img
+                        className="pedido__img"
+                        src={data[0].src}
+                        alt={data[0].title}
+                    />
+                    <div className="pedido__line"></div>
+
+                    <div className="pedido__subcontainer">
+                        <div className="pedido__description">
+                            <p className="pedido__title">{data[0].title}</p>
+                            <p className="pedido__unit">
+                                Unidad: {data[0].unidad}
+                            </p>
+
+                            {data[0].precio ? (
+                                <p className="pedido__price">
+                                    <span className="pedido__price__extra">
+                                        $
+                                    </span>
+                                    {data[0].precio}
+                                    <span className="product__price__extra">
+                                        {' '}
+                                        por unidad
+                                    </span>
+                                </p>
+                            ) : (
+                                <div></div>
+                            )}
+                        </div>
+
+                        <div className="pedido__buttons">
+
+                            <div className="pedido__btn__quantity__box">
+                                <button
+                                    className="btn__quantity pedido__btn__control btn__decrease"
+                                >
+                                    <MinusIcon className="minus__icon btn__quantity pedido__btn__control" />
+                                </button>
+                                <p className="product__quantity">2</p>
+                                <button
+                                    className="pedido__btn__control btn__increase btn__quantity"
+                                >
+                                    <PlusIcon className="plus__icon btn__quantity pedido__btn__control" />
+                                </button>
+                            </div>
+
+                            <button className="btn__quantity btn__delete">
+                                Borrar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="pedido" key={data[0].id}>
+                    <img
+                        className="pedido__img"
+                        src={data[0].src}
+                        alt={data[0].title}
+                    />
+                    <div className="pedido__line"></div>
+
+                    <div className="pedido__subcontainer">
+                        <div className="pedido__description">
+                            <p className="pedido__title">{data[0].title}</p>
+                            <p className="pedido__unit">
+                                Unidad: {data[0].unidad}
+                            </p>
+
+                            {data[0].precio ? (
+                                <p className="pedido__price">
+                                    <span className="pedido__price__extra">
+                                        $
+                                    </span>
+                                    {data[0].precio}
+                                    <span className="product__price__extra">
+                                        {' '}
+                                        por unidad
+                                    </span>
+                                </p>
+                            ) : (
+                                <div></div>
+                            )}
+                        </div>
+
+                        <div className="pedido__buttons">
+
+                            <div className="pedido__btn__quantity__box">
+                                <button
+                                    className="btn__quantity pedido__btn__control btn__decrease"
+                                >
+                                    <MinusIcon className="minus__icon btn__quantity pedido__btn__control" />
+                                </button>
+                                <p className="product__quantity">2</p>
+                                <button
+                                    className="pedido__btn__control btn__increase btn__quantity"
+                                >
+                                    <PlusIcon className="plus__icon btn__quantity pedido__btn__control" />
+                                </button>
+                            </div>
+
                             <button className="btn__quantity btn__delete">
                                 Borrar
                             </button>
@@ -109,12 +286,12 @@ function Pedidos() {
 
             <div className="pedidos__form__container">
                 <form className="pedidos__form">
-                    <h1 class="signup1">SIGN UP</h1>
+                    <h3 class="pedidos__form__message">Registrá tus datos</h3>
                         <input name="nombre" type="text" placeholder="Nombre*" class="pedidos__form__text"/>
 
                         <input name="apellido" type="text" placeholder="Apellido*" class="pedidos__form__text"/>
                         
-                        <input name="correo" type="email" placeholder="Correo*" class="pedidos__form__text"/>
+                        <input name="mail" type="email" placeholder="Correo*" class="pedidos__form__text"/>
 
                         <input name="telefono" type="tel" placeholder="Teléfono*" class="pedidos__form__text"/>
 
@@ -124,20 +301,21 @@ function Pedidos() {
 
                         <input name="direccion" type="text" placeholder="Dirección*" class="pedidos__form__text"/>
 
-                        <textarea id="direccion" name="direccion" placeholder="Dirección*"></textarea>
+                        <textarea id="direccion" name="direccion" placeholder="Detalles dirección" class="pedidos__form__text"></textarea>
 
                         <input name="CDP" type="text" placeholder="CDP*" class="pedidos__form__text"/>
-
-                        <input name="email" type="text" placeholder="Email*" class="pedidos__form__text"/>
-
-                        <input name="email" type="text" placeholder="Email*" class="pedidos__form__text"/>
                         
                         
                     
                     <button class="btn">Sign Up</button>
                 </form>
-            </div>
 
+                
+            </div>
+            
+            <footer className="footer">
+                <p>© Derechos reservados</p>
+            </footer>                    
         </div>
     );
 }
